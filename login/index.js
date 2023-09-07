2d018ed6d96f45d495cedc6ba97c30bb9810cb79
Christian Ahamefule <chrisaham96@gmail.com>
2023-09-07 20:42:05 +0100

    Enhance HTML, Responsive Styling, and Optimize JavaScript
    Refined HTML structure, improved styling for responsiveness, and optimized JavaScript code for enhanced performance

--- login/index.js
+++ login/index.js
@@ -1,8 +1,8 @@
-const eye = document.querySelector('#eye');
-const username = document.querySelector("#userName");
-const password = document.querySelector('#password');
+const eye = document.querySelector("#eye");
+const password = document.querySelector("#password");
 const loginButton = document.querySelector("#loginButton");
-const info = document.querySelector("#info")
+const info = document.querySelector(".info");
+const username = document.querySelector("#username");
 
 const users = [
     {
@@ -27,35 +27,30 @@ const users = [
     }
 ]
 
-
-// initializing buttons
-eye.onclick = show;
-loginButton.onclick = check;
 username.onclick = reset;
 password.onclick = reset;
-
-function show(){
-    if(password.type === "password"){
-        password.type = "type";
-    } else {
-        password.type = "password";
-    }
-    this.classList.toggle('fa-eye-slash');
-}
-
-function check (){
-    const user = users.find(user => user.name === username.value);
-
-    if (user && user.password === password.value){
-        window.alert("Welcome "+ username.value +"!");
-    } else {
-        username.value = "";
-        password.value = '';
-        info.style.display = "block";
-        info.innerText = "Invalid username or password";
-    }
-}
 
 function reset (){
     info.style.display = 'block' ? "none" : "block";
 }
+
+function clearField(){
+    username.value = '';
+    password.value = '';
+}
+
+eye.addEventListener("click", function () { 
+    password.type = password.type === 'password' ? "text":"password";
+    this.classList.toggle ('fa-eye-slash');
+});
+
+loginButton.addEventListener("click", function(){
+    let user = users.find(user => user.name === username.value);
+    if (user && user.password === password.value){
+        window.alert("welcome "+username.value+"!");
+    } else {
+        info.style.display = "block";
+        info.innerHTML = 'Invalid username or password';
+    }
+    clearField()
+});
